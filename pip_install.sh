fb583279cf6f0dacbdbefc7e62e92c06e99ff274
Jonathan Sedar <jon@sedar.co>
2022-03-21 12:00:13 +0300

    handful of updates to pin packages and switch to the released version of oreum_core and always ignore local

--- pip_install.sh
+++ pip_install.sh
@@ -1,3 +1,2 @@
 pip install -r pip_requirements.txt
-# pip install -e ~/workspace/oreum/oreum_core  # optional install from local dev
 pip-licenses -saud -f markdown --output-file LICENSES_THIRD_PARTY.md
